dbab2dd52be078bb0fd3ee3edab3759aa4f960ba
Timothée Boucher <timothee@gmail.com>
2012-10-06 19:52:44 -0700

    Add ability to unmute ads or mute a show/movie.
    It will still mute when a commercial break comes up and unmute when the show comes back on, it's just that it gives you the possibility to listen to an ad if it seems interesting. (previously, it would re-mute in the next second)

--- mutehulu.js
+++ mutehulu.js
@@ -1,16 +1,20 @@
 (function() {
 	var player;
+	var lastStateSeen;
 
 	window.setInterval(function() {
 		if (!player) {
 			player = document.getElementById('player');
 		} else {
-			if (player.getCurrentState().subState == "content") {
-				if (player.getVolume() == 0) {
-					player.unMute();
+			if (player.getCurrentState().subState != lastStateSeen) {
+				if (player.getCurrentState().subState == "content") {
+					if (player.getVolume() == 0) {
+						player.unMute();
+					}
+				} else if (player.getVolume()) {
+					player.mute();
 				}
-			} else if (player.getVolume()) {
-				player.mute();
+				lastStateSeen = player.getCurrentState().subState;
 			}
 		}
 	}, 1000);
