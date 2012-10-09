8c22df7e77aff351ce63946fc02111cfa98c0749
Timothée Boucher <timothee@gmail.com>
2012-10-08 21:52:09 -0700

    Add black mask on top of video during ads

--- mutehulu.js
+++ mutehulu.js
@@ -1,18 +1,33 @@
 (function() {
 	var player;
 	var lastStateSeen;
+	var mask = document.createElement('div');
+
+	function setMask() {
+		mask.style.position = "absolute";
+		mask.style.width = player.offsetWidth + "px";
+		mask.style.height = player.offsetHeight + "px";
+		mask.style.top = player.offsetTop + "px";
+		mask.style.left = player.offsetLeft + "px";
+		mask.style.backgroundColor = "black";
+		mask.style.zIndex = 10000;
+		mask.innerHTML = "&nbsp;";
+	}
 
 	window.setInterval(function() {
 		if (!player) {
 			player = document.getElementById('player');
 		} else {
+			setMask();
 			if (player.getCurrentState().subState != lastStateSeen) {
 				if (player.getCurrentState().subState == "content") {
 					if (player.getVolume() == 0) {
 						player.unMute();
+						player.parentElement.removeChild(mask);
 					}
 				} else if (player.getVolume()) {
 					player.mute();
+					player.parentElement.appendChild(mask);
 				}
 				lastStateSeen = player.getCurrentState().subState;
 			}
