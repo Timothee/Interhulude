ed4a3c1c511419975ae43cb5ce15585a4a2baed5
Timothée Boucher <timothee@gmail.com>
2012-11-05 08:54:05 -0800

    Add message with show name. Some refactoring as well

--- interhulude.js
+++ interhulude.js
@@ -3,31 +3,49 @@
 	var lastStateSeen;
 	var mask = document.createElement('div');
 
+	function log(message) {
+		console.log("[Interhulude] " + message);
+	}
+
+	function maskVideo() {
+		setMask();
+		log("Masking video");
+		player.mute();
+		player.parentElement.appendChild(mask);
+	}
+
+	function unmaskVideo() {
+		log("Unmasking video");
+		player.unMute();
+		player.parentElement.removeChild(mask);
+	}
+
 	function setMask() {
+		mask.id = "InterhuludeMask";
 		mask.style.position = "absolute";
 		mask.style.width = player.offsetWidth + "px";
 		mask.style.height = player.offsetHeight + "px";
 		mask.style.top = player.offsetTop + "px";
 		mask.style.left = player.offsetLeft + "px";
 		mask.style.backgroundColor = "black";
+		mask.style.color = "white";
 		mask.style.zIndex = 10000;
-		mask.innerHTML = "&nbsp;";
+		var showName = document.getElementsByClassName('show-title')[0] ? document.getElementsByClassName('show-title')[0].innerHTML : "Your show";
+		mask.innerHTML = "<h1>" + showName + " will return in a moment.</h1>" + "<p>(click to dismiss)</p>";
+		mask.onclick = unmaskVideo;
 	}
 
 	window.setInterval(function() {
 		if (!player) {
 			player = document.getElementById('player');
 		} else {
-			setMask();
-			if (player.getCurrentState().subState != lastStateSeen) {
+			if (player.getCurrentState && player.getCurrentState().subState != lastStateSeen) {
 				if (player.getCurrentState().subState == "content") {
 					if (player.getVolume() == 0) {
-						player.unMute();
-						player.parentElement.removeChild(mask);
+						unmaskVideo();
 					}
-				} else if (player.getVolume()) {
-					player.mute();
-					player.parentElement.appendChild(mask);
+				} else if (player.getVolume && player.getVolume()) {
+					maskVideo();
 				}
 				lastStateSeen = player.getCurrentState().subState;
 			}
